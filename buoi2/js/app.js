10cac49365c59a26b95a62cc5058e5c6a11a0d22
NGUYEN TIEN DAT <NTIENDAT>
2023-03-13 18:28:32 +0700

    ph39907

--- buoi2/js/app.js
+++ buoi2/js/app.js
@@ -1,9 +1,21 @@
 function menu() {
     var x = document.getElementById("menuMB");
-    
+
     if (x.className === "menu-2") {
         x.className += " show";
     } else {
         x.className = "menu-2";
     }
 }
+let imgBanner = document.querySelector('.img-banner');
+ let img1 = document.querySelector('.img-1 img');
+  let img2 = document.querySelector('.img-2 img'); 
+  let showImage = function () {
+     setTimeout(function () { img1.style.opacity = '0'; 
+     img2.style.opacity = '1'; hideImage();
+     }, 8000); }
+      let hideImage = function () { 
+        setTimeout(function () { img2.style.opacity = '0'; 
+        img1.style.opacity = '1'; showImage(); }, 8000); 
+    }
+     showImage();
